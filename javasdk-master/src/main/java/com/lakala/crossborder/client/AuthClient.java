e718669dfd204f073e490960e5b7c14b36bbab40
905001561 <905001561@qq.com>
2018-12-19 16:49:49 +0800

    787

--- javasdk-master/src/main/java/com/lakala/crossborder/client/AuthClient.java
+++ javasdk-master/src/main/java/com/lakala/crossborder/client/AuthClient.java
@@ -37,7 +37,7 @@ public class AuthClient {
      * @throws LklClientException
      */
     public AuthRes auth(AuthReq authReq, LklCrossPaySuperReq dataHead) throws LklClientException {
-        System.out.println("test_dome____11111555555");
+        System.out.println("test_dome____1111155555566666");
     	AuthRes authRes = null;
         LklCrossPayEncryptReq req = LklMsgUtil.encryptMsg(authReq, dataHead);
         try {
